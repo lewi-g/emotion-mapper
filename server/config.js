8feaf80fed8a24338c3e8bfdaf3009f91308ac9c
Lewi Gilamichael <dr.lewi@gmail.com>
2017-08-07 16:58:38 -0400

    config set up with mlab

---
+++ server/config.js
@@ -0,0 +1,8 @@
+'use strict';
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+  global.DATABASE_URL ||
+  'mongodb://localhost/emotions';
+exports.TEST_DATABASE_URL = (
+  process.env.TEST_DATABASE_URL ||
+  'mongodb://localhost/test-emotions');
+exports.PORT = process.env.PORT || 8080;
