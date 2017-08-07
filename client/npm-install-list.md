e9844cb11126520c85bd6926e1241c705ae82ae2
mscottcam <mscottcam@gmail.com>
2017-08-07 14:14:05 -0500

    user-login initial setup

---
+++ client/npm-install-list.md
@@ -0,0 +1,6 @@
+react
+react-redux
+react-scripts
+react-dom
+redux
+redux-thunk
---
+++ client/src/components/user-login.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default class UserLogin extends React.Component {
+
+  submitUser(event) {
+    event.preventDefault()
+    const userName = this.input
+    const passWord = this.passwordInput
+    // dispatch to action
+  }
+
+  render () {
+    return {
+      <div className="user-login">
+        <form onSubmit={e => this.submitUser(e)}>
+          <fieldset>
+            <input type="text" name="username" id="username"
+              className="username-input" required
+              ref={input => this.input = input} />
+            <input type="text" name="password" id="password"
+              className="password-input" required
+              ref={passwordInput => this.input = passwordInput} />
+               <button type="submit" id="user-submit-button" className="button" name="submit">Cheese</button>
+          </fieldset>
+        </form>
+      </div>
+    }
+  }
+}
+
+
+// how to grab inputs?
+// connections
