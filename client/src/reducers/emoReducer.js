b1621ae4006b8a87f2673d550a98e7079e790462
Lewi Gilamichael <dr.lewi@gmail.com>
2017-08-08 16:03:48 -0400

    emoReducer updated

--- client/src/reducers/emoReducer.js
+++ client/src/reducers/emoReducer.js
@@ -2,27 +2,38 @@ import { GO_DEEPER1, G0_DEEPER2, FINAL_SELECTION, SUBMIT_ENTRY } from '../action
 import { emoRubric } from '../emotionRubric.js';
 
 const initialState = {
-   emoTier1: 'surprised',
-   emoTier2: 'amazed',
+   emoTier1: 'bad',
+   emoTier2: 'disgusted',
    emoTier3: '',
+   emoOptions: []
    //userComment: ''
 }
 
 // find state.emotier1 in emoRubric 
 // and take the array of keys and push to components
+const emoTier2Choices= Object.keys(emoRubric)
+.find((emoTier1) => Object.keys(state.emoTier1))
 
 // find state.emotier2 in emoRubric
 // and take the array and push to components
+const emoTier3Choices= Object.keys(emoRubric.emoTier2)
 
 export const emoReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'GO_Deeper':
       return {
       ...state,
-      emoTier1: action.emotionSelection
+      emoTier1: action.emotionSelection,
+      emoOptions: emoTier2Choices
+      }
+    case 'GO_Deeper2':
+      return {
+      ...state,
+      emoTier2: action.emotionSelection,
+      emoOptions: emoTier3Choices
         // display the next level of emotion components
       }
-      case 'GO_Deeper2':
+    case 'SELECT_FINAL':
       return {
       ...state,
       emoTier2: action.emotionSelection
