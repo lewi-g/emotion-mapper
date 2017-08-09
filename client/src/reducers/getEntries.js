b524dd46a9cae5895d655e990fc8ae3df6791d42
Lewi Gilamichael <dr.lewi@gmail.com>
2017-08-09 14:59:18 -0400

    added getEntries reducer

---
+++ client/src/reducers/getEntries.js
@@ -0,0 +1,38 @@
+import {
+  FETCH_USER_ENTRIES_SUCCESS,
+  FETCH_USER_ENTRIES_REQUEST,
+  FETCH_USER_ENTRIES_ERROR
+} from "../actions/cheese";
+
+const initialState = {
+  userEntries: [],
+  loading: false,
+  error: null
+};
+
+const userEntries = (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_USER_ENTRIES_ERROR":
+      return {
+        ...state,
+        error: action.error,
+        loading: false
+      };
+    case "FETCH_USER_ENTRIES_REQUEST":
+      return {
+        ...state,
+        loading: true
+      };
+    case "FETCH_USER_ENTRIES_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        userEntries: action.userEntries
+      };
+    default:
+      return state;
+  }
+};
+
+export default userEntries;
