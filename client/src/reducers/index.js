332969c6f112fd657a0279bbfbd2ed1f709fd88d
Lewi Gilamichael <dr.lewi@gmail.com>
2017-08-09 15:00:35 -0400

    updated root reducer to include getEntries

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,10 +2,11 @@ import {combineReducers} from 'redux'
 
 import {emoReducer} from './emoReducer'
 import postEntry  from './postEntry'
+import getEntries from './getEntries'
 
 
 const rootReducer = combineReducers({
-  emoReducer, postEntry
+  emoReducer, postEntry, getEntries
 })
 
 export default rootReducer
