53d27d640de2592d94046422614b6362d6b487f7
mscottcam <mscottcam@gmail.com>
2017-11-07 13:44:12 -0600

    deletes emotion parent and moves route for first emotion

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 import './App.css'
-import EmotionParent from './components/emotion-parent'
+import FirstEmotion from './components/first-emotion'
 import UserHistory from './components/user-history'
 
 export default class App extends React.Component {
@@ -11,12 +11,13 @@ export default class App extends React.Component {
       <Router>
         <div className="App">
           <header>
-            <h1><Link to="/emotions">Man Up!</Link></h1>
-            <h2><Link to="/user-history">See My History</Link></h2>
+            <h1><Link to="/">Dig Deep</Link></h1>
           </header>
           <main>
             <div className="main-div">
-              <Route path="/emotions" component={EmotionParent} />
+              <h2><Link to="/emotions">Start New Entry</Link></h2>
+              <h2><Link to="/user-history">See My History</Link></h2>
+              <Route path="/emotions" component={FirstEmotion} />
               <Route exact path="/user-history" component={UserHistory} />
             </div>
           </main>
